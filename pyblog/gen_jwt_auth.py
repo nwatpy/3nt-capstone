e4813859b2cc464200bf9273a646a6df6bec989e
Nick Wat <nicholas.waterman@libertymutual.com>
2022-12-03 18:37:15 -0500

    gen_jwt_auth.py

---
+++ pyblog/gen_jwt_auth.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+import requests
+import pprint
+from dotenv import dotenv_values
+
+CREDS = dotenv_values('.env')
+
+
+def get_jwt_auth(CREDS):
+    post = {
+        "username": CREDS["user"],
+        "password": CREDS["password"]
+    }
+    try:
+        r = requests.post(CREDS["wp_jwt_auth_url"], json=post)
+        if not r.status_code == 200:
+            return None
+    except requests.exceptions.ConnectionError:
+        return None
+    except requests.exceptions.Timeout:
+        return None
+    r = r.json()
+    return r
+
+def pprint_r(r):  # pragma: no cover
+    with open('./data/jwt-auth.json', 'w') as f:
+        PP = pprint.PrettyPrinter(indent=4, stream=f)
+        PP.pprint(r)
+
+if __name__ == "__main__":
+    r = get_jwt_auth(CREDS); pprint_r(r)
