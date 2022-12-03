8abfad9cc9c27471d79191e68fa9042e995c3ff8
nwatpy <ndwman42@gmail.com>
2022-12-03 06:19:14 -0500

    pyblog interface updates

--- pyblog/pyblog.py
+++ pyblog/pyblog.py
@@ -7,7 +7,6 @@ import yaml
 import datetime as DT
 from dotenv import dotenv_values
 from sys import exit
-from parser import ParserError
 
 CREDS = dotenv_values('.env')
 
@@ -18,17 +17,16 @@ def parse_args():  # pragma: no cover
             Pyblog written by Team N3T from the IEA Cohort 08.")
     parser.add_argument('--blog', action='store', help='use \
                         --blog= with the following parameters \
-                        to interact with wordpress: read, write')
+                        to interact with Pyblog CLI: read, write')
     parser.add_argument('--data', action='store', help='use \
                         --data= with the following parameters \
-                        to view wordpress rest api output in \
-                        specified data format: json, yaml')
+                        to view and save wordpress rest api output in \
+                        specified data format: json, yaml \
+                        Example: --blog=read --data=json')
     args = parser.parse_args()
     varsargs = vars(args)
     try:
         argsvars = lower_argsvars(varsargs)
-    except ParserError:
-        return None, None
     except AttributeError:
         return None, None
     except OverflowError:
@@ -42,17 +40,13 @@ def parse_args():  # pragma: no cover
         if args.data == "yaml":
             if args.blog == "read":
                 return argsvars["blog"], argsvars["data"]
-        else:
-            data = None
     if args.blog:
         if args.blog == "read":
             return argsvars["blog"], None
         if args.blog == "write":
             return argsvars["blog"], None
-        else:
-            pass
     else:
-        return None, data
+        return None, None
 
 
 def lower_argsvars(varsargs):  # pragma: no cover
@@ -80,20 +74,22 @@ def pprint_r(r):  # pragma: no cover
         PP = pprint.PrettyPrinter(indent=4, stream=f)
         PP.pprint(r)
 
+
 def yamlprint_r(r):  # pragma: no cover
     with open('./data/r.yaml', 'w') as f:
         yaml.dump(r, f, allow_unicode=True)
     with open('./data/r.yaml', 'r') as f:
         print(f.read())
 
+
 def jsonprint_r(r):  # pragma: no cover
     PP = pprint.PrettyPrinter(indent=4)
     PP.pprint(r)
 
-def get_iso_date():
+
+def get_date():  # pragma: no cover
     date = DT.datetime.now()
-    iso_date = date.isoformat(timespec='seconds')
-    return iso_date
+    return date.isoformat(timespec='seconds')
 
 
 def run_read(CREDS, data):  # pragma: no cover
@@ -104,71 +100,78 @@ def run_read(CREDS, data):  # pragma: no cover
     if data == "json":
         pprint_r(r)
         jsonprint_r(r)
+        ("Data saved to ./data/r.json")
         exit()
     if data == "yaml":
         yamlprint_r(r)
+        ("Data saved to ./data/r.yaml")
         exit()
     else:
         run_read_CLI(r)
 
+
 def run_read_CLI(r):
     while True:
-        print("Please supply parameters for how you would like to view your data: \
-        json, yaml, \
-        write (switch to write mode), \
+        print("Choose how to display data:\
+        json, yaml\
+        write (switch to write mode)\
         exit (quit Pyblog CLI)")
         CLI_input = input()
-        if CLI_input == "json":
+        if CLI_input.lower() == "json":
             jsonprint_r(r)
-        if CLI_input == "yaml":
+            print("Data not saved.  Use --data=json\
+            to save the output to ./data/r.json")
+        if CLI_input.lower() == "yaml":
             yamlprint_r(r)
+            print("Data saved to ./data/r.yaml")
         else:
             if CLI_input == "write":
                 run_write_CLI(CREDS)
             if CLI_input == "exit":
                 exit()
 
+
 def run_write_CLI(CREDS):
     post = {
         "status": "publish",
-        "date": get_iso_date(),
-        "categories": 1
-        }
+        "date": get_date(),
+    }
     while True:
-        print("Please select from the following keys parameters \
-              in order to supply a value in order to construct post data to send to wordpress: \
-        title, content, post (show post data before publishing) \
+        print("Please select from the following:\
+        title (create title for your post)\
+        content (write your post)\
+        post (show post data before publishing)\
         publish (post to wordpress)\
-        read (switch to read mode), \
+        read (switch to read mode)\
         exit (quit Pyblog CLI)")
         CLI_input = input()
-        if CLI_input == "title":
+        if CLI_input.lower() == "title":
             print("Provide title for your post:")
             CLI_input = input()
             post.setdefault("title", CLI_input)
-        if CLI_input == "content":
+        if CLI_input.lower() == "content":
             print("Provide content for your post:")
             CLI_input = input()
             post.setdefault("content", CLI_input)
-        if CLI_input == "post":
+        if CLI_input.lower() == "post":
             print(post)
-        if CLI_input == "publish":
-                try:
-                    r = requests.post(CREDS["wp_posts_url"], auth=(CREDS["user"], CREDS["password"]), json=post)
-                    if not r.status_code == 200:
-                        print("Could not connect to wordpress.")
-                except requests.exceptions.ConnectionError:
+        if CLI_input.lower() == "publish":
+            try:
+                r = requests.post(CREDS["wp_posts_url"], auth=(
+                    CREDS["user"], CREDS["password"]), data=post)
+                if not r.status_code == 200:
                     print("Could not connect to wordpress.")
-                except requests.exceptions.Timeout:
-                    print("Could not connect to wordpress.")
-                print(r)
+            except requests.exceptions.ConnectionError:
+                print("Could not connect to wordpress.")
+            except requests.exceptions.Timeout:
+                print("Could not connect to wordpress.")
+            print(r)
         else:
-            if CLI_input == "read":
+            if CLI_input.lower() == "read":
                 data = None
                 run_read(CREDS, data)
-            if CLI_input == "exit":
+            if CLI_input.lower() == "exit":
                 exit()
-
 
 
 def run_pyblog():  # pragma: no cover
@@ -178,9 +181,8 @@ def run_pyblog():  # pragma: no cover
     if blog == "write":
         run_write_CLI(CREDS)
     else:
-        print("Please specify --blog=read or --blog=write.  \
-            Please use -h or --help on the command line \
-                for more information on how to use Pyblog")
+        print("Please specify --blog=read or --blog=write. \
+        Please use -h or --help for more information on this tool")
 
 
 if __name__ == "__main__":
