c2b8bcdc46fdd90270c1da3e8fbff9a6da8e1977
nwatpy <ndwman42@gmail.com>
2022-12-08 03:02:31 -0500

    small pyblog refactor for added unit tests

--- pyblog/pyblog.py
+++ pyblog/pyblog.py
@@ -11,8 +11,16 @@ import datetime as DT
 from dotenv import load_dotenv
 from sys import exit
 
+"""
+GLOBAL AND ENVIRONMENT VARIABLES
+"""
+
 load_dotenv()
 CREDS = os.environ
+
+"""
+ARGPARSER FUNCTIONS
+"""
 
 
 def parse_args():  # pragma: no cover
@@ -72,18 +80,70 @@ def lower_argsvars(varsargs):  # pragma: no cover
         return varsargs
 
 
-def get_wp_r(CREDS):
-    try:
-        r = requests.get(CREDS["wp_posts_url"], auth=(
-            CREDS["user"], CREDS["password"]))
-        if not r.status_code == 200:
-            return None
-    except requests.exceptions.ConnectionError:
-        return None
-    except requests.exceptions.Timeout:
-        return None
-    r = r.json()
-    return r
+"""
+STRING RETURNING FUNCTIONS
+"""
+
+
+def run_pyblog_specify_blog_data():
+    return "Please specify --blog=read or --blog=write. \
+        Please use -h or --help for more information on this tool"
+
+
+def run_write_CLI_main_menu():
+    return "Please select from the following:\
+        title (create title for your post)\
+        content (write your post)\
+        post (show post data before publishing)\
+        publish (post to wordpress)\
+        template (publishes designated template file)\
+        read (switch to read mode)\
+        exit (quit Pyblog CLI)"
+
+
+def run_write_title_menu():
+    return "Provide a title for your post:"
+
+
+def run_write_content_menu():
+    return "Provide content for your post:"
+
+
+def run_write_ask_template_data():
+    return "Is template file yaml or plain text?\
+    Type yaml or plain:"
+
+
+def run_write_ask_template_filepath():
+    return "Provide filepath to template:"
+
+
+def run_read_main_menu():
+    return "Choose how to display data:\
+        json, yaml\
+        write (switch to write mode)\
+        exit (quit Pyblog CLI)"
+
+
+def run_read_connection_error():
+    return "Could not connect to wordpress."
+
+
+def run_read_display_json():
+    return "Displaying newest post data.\
+        Entire response saved to ./data/r.json"
+
+
+def run_read_display_yaml():
+    return "Displaying newest post data.\
+        Entire response saved to ./data/r.yaml\
+        Clean response saved to ./data/clean.yaml\
+        yaml data displayed saved to ./data/cleaner.yaml"
+
+
+"""
+PPRINT AND OTHER DATA LOAD/PRINT FUNCTIONS
+"""
 
 
 def pprint_r(r):  # pragma: no cover
@@ -97,17 +157,7 @@ def yamlprint_r(r):  # pragma: no cover
         yaml.dump(r, f, allow_unicode=True)
 
 
-def parse_post_id():
-    with open('./data/cleaner.yaml', 'r') as f:
-        post_id = yaml.safe_load(f)
-    with open('./data/parse_post_id.json', 'w') as f:
-        json.dump(post_id, f)
-    with open('./data/parse_post_id.json', 'r') as f:
-        post_id = json.load(f)
-    return int(post_id["id"])
-
-
-def yaml_clean_print(post_id):
+def yaml_clean_print(post_id):  # pragma: no cover
     with open('./data/r.yaml', 'r') as f:
         clean_yaml = ruamel.yaml.round_trip_load(f)
     with open('./data/clean.yaml', 'w+') as f:
@@ -121,17 +171,17 @@ def yaml_clean_print(post_id):
                 clean_yaml, stream=f, block_seq_indent=1)
 
 
-def get_cleaner_yaml(r):
+def get_cleaner_yaml(r):  # pragma: no cover
     with open('./data/cleaner.yaml', 'w') as f:
         yaml.dump(r, f, allow_unicode=True)
 
 
-def display_cleaner_yaml():
+def display_cleaner_yaml():  # pragma: no cover
     with open('./data/cleaner.yaml', 'r') as f:
         print(f.read())
 
 
-def remove_links_json(r):
+def remove_links_json(r):  # pragma: no cover
     keys = '_links'
     return {k: v for k, v in r[0].items() if k not in keys}
 
@@ -149,173 +199,38 @@ def jsonprint_r(r):  # pragma: no cover
     PP.pprint(r)
 
 
+def parse_post_id():  # pragma: no cover
+    with open('./data/cleaner.yaml', 'r') as f:
+        post_id = yaml.safe_load(f)
+    with open('./data/parse_post_id.json', 'w') as f:
+        json.dump(post_id, f)
+    with open('./data/parse_post_id.json', 'r') as f:
+        post_id = json.load(f)
+    return int(post_id["id"])
+
+
 def get_date():  # pragma: no cover
     date = DT.datetime.now()
     return date.isoformat(timespec='seconds')
 
 
-def run_read(CREDS, data):  # pragma: no cover
-    r = get_wp_r(CREDS)
-    if r is None:
-        print("Could not connect to wordpress.")
-        exit()
-    if data == "json":
-        pprint_r(r)
-        jsonprint_r(r)
-        print("Displaying newest post data.\
-        Entire response saved to ./data/r.json")
-        exit()
-    if data == "yaml":
-        yamlprint_r(r)
-        try:
-            r = remove_links_json(r)
-        except KeyError:
-            pass
-        get_cleaner_yaml(r)
-        post_id = parse_post_id()
-        try:
-            yaml_clean_print(post_id)
-        except KeyError:
-            pass
-        display_cleaner_yaml()
-        print("Displaying newest post data.\
-        Entire response saved to ./data/r.yaml\
-        Clean response saved to ./data/clean.yaml\
-        yaml data displayed saved to ./data/cleaner.yaml")
-        exit()
-    else:
-        run_read_CLI(r)
+"""
+GET, POST AND SUPPORTING CLI FUNCTIONS
+"""
 
 
-def run_read_CLI(r):
-    while True:
-        print("Choose how to display data:\
-        json, yaml\
-        write (switch to write mode)\
-        exit (quit Pyblog CLI)")
-        CLI_input = input()
-        if CLI_input.lower() == "json":
-            pprint_r(r)
-            jsonprint_r(r)
-            print("Displaying newest post data.\
-            Entire response saved to ./data/r.json")
-        if CLI_input.lower() == "yaml":
-            yamlprint_r(r)
-            try:
-                r = remove_links_json(r)
-            except KeyError:
-                pass
-            get_cleaner_yaml(r)
-            post_id = parse_post_id()
-            try:
-                yaml_clean_print(post_id)
-            except KeyError:
-                pass
-            display_cleaner_yaml()
-            print("Displaying newest post data.\
-            Entire response saved to ./data/r.yaml\
-            Clean response saved to ./data/clean.yaml\
-            yaml data displayed saved to ./data/cleaner.yaml")
-        else:
-            if CLI_input.lower() == "write":
-                run_write_CLI(CREDS)
-            if CLI_input.lower() == "exit":
-                exit()
-
-
-def run_write_CLI(CREDS):
-    post = {
-        "date": get_date(),
-        "status": "publish",
-        "format": "standard"
-    }
-    while True:
-        print("Please select from the following:\
-        title (create title for your post)\
-        content (write your post)\
-        post (show post data before publishing)\
-        publish (post to wordpress)\
-        template (publishes designated template file)\
-        read (switch to read mode)\
-        exit (quit Pyblog CLI)")
-        CLI_input = input()
-        if CLI_input.lower() == "title":
-            print("Provide title for your post:")
-            CLI_input = input()
-            post.setdefault("title", CLI_input)
-        if CLI_input.lower() == "content":
-            print("Provide content for your post:")
-            CLI_input = input()
-            post.setdefault("content", CLI_input)
-        if CLI_input.lower() == "post":
-            PP = pprint.PrettyPrinter(indent=4)
-            PP.pprint(post)
-        if CLI_input.lower() == "publish":
-            wp_publish(CREDS, post)
-        if CLI_input.lower() == "template":
-            print("Is template file yaml or plain text? Type yaml or plain:")
-            CLI_input = input()
-            if CLI_input == "yaml":
-                print("Provide filepath to template:")
-                CLI_input = input()
-                post_template = load_template(CLI_input)
-                post.setdefault("title", post_template["title"])
-                post.setdefault("content", post_template["content"])
-                wp_publish(CREDS, post)
-            if CLI_input == "plain":
-                print("Provide filepath to template:")
-                CLI_input = input()
-                template = CLI_input
-                publish_plaintext(template)
-
-        else:
-            if CLI_input.lower() == "read":
-                data = None
-                run_read(CREDS, data)
-            if CLI_input.lower() == "exit":
-                exit()
-
-
-def load_template(CLI_input):
-    with open(CLI_input, 'r') as f:
-        post_template = yaml.safe_load(f)
-    with open('./data/post_template.json', 'w') as f:
-        json.dump(post_template, f)
-    with open('./data/post_template.json', 'r') as f:
-        post_template = json.load(f)
-    return post_template
-
-
-def publish_template(template):
-    post = {
-        "date": get_date(),
-        "status": "publish",
-        "format": "standard"
-    }
-    with open(template, 'r') as f:
-        post_template = yaml.safe_load(f)
-    with open('./data/post_template.json', 'w') as f:
-        json.dump(post_template, f)
-    with open('./data/post_template.json', 'r') as f:
-        post_template = json.load(f)
-    post.setdefault("title", post_template["title"])
-    post.setdefault("content", post_template["content"])
-    wp_publish(CREDS, post)
-
-
-def publish_plaintext(template):
-    post = {
-        "date": get_date(),
-        "status": "publish",
-        "format": "standard"
-    }
-    with open(template, 'r') as f:
-        lines = f.readlines()
-        post_template = [line.strip() for line in lines]
-    post.setdefault("title", post_template.pop(0))
-    post_template = '\n '.join(post_template)
-    post.setdefault("content", post_template)
-    wp_publish(CREDS, post)
+def get_wp_r(CREDS):
+    try:
+        r = requests.get(CREDS["wp_posts_url"], auth=(
+            CREDS["user"], CREDS["password"]))
+        if not r.status_code == 200:
+            return None
+    except requests.exceptions.ConnectionError:
+        return None
+    except requests.exceptions.Timeout:
+        return None
+    r = r.json()
+    return r
 
 
 def wp_publish(CREDS, post):
@@ -337,6 +252,157 @@ def wp_publish(CREDS, post):
         return "Could not connect to wordpress."
 
 
+def run_read(CREDS, data):  # pragma: no cover
+    r = get_wp_r(CREDS)
+    if r is None:
+        print(run_read_connection_error())
+        exit()
+    if data == "json":
+        pprint_r(r)
+        jsonprint_r(r)
+        print(run_read_display_json())
+        exit()
+    if data == "yaml":
+        yamlprint_r(r)
+        try:
+            r = remove_links_json(r)
+        except KeyError:
+            pass
+        get_cleaner_yaml(r)
+        post_id = parse_post_id()
+        try:
+            yaml_clean_print(post_id)
+        except KeyError:
+            pass
+        display_cleaner_yaml()
+        print(run_read_display_yaml())
+        exit()
+    else:
+        run_read_CLI(r)
+
+
+def run_read_CLI(r):  # pragma: no cover
+    while True:
+        print(run_read_main_menu())
+        CLI_input = input()
+        if CLI_input.lower() == "json":
+            pprint_r(r)
+            jsonprint_r(r)
+            print(run_read_display_json())
+        if CLI_input.lower() == "yaml":
+            yamlprint_r(r)
+            try:
+                r = remove_links_json(r)
+            except KeyError:
+                pass
+            get_cleaner_yaml(r)
+            post_id = parse_post_id()
+            try:
+                yaml_clean_print(post_id)
+            except KeyError:
+                pass
+            display_cleaner_yaml()
+            print(run_read_display_yaml())
+        else:
+            if CLI_input.lower() == "write":
+                run_write_CLI(CREDS)
+            if CLI_input.lower() == "exit":
+                exit()
+
+
+def run_write_CLI(CREDS):  # pragma: no cover
+    post = {
+        "date": get_date(),
+        "status": "publish",
+        "format": "standard"
+    }
+    while True:
+        print(run_write_CLI_main_menu())
+        CLI_input = input()
+        if CLI_input.lower() == "title":
+            print(run_write_title_menu())
+            CLI_input = input()
+            post.setdefault("title", CLI_input)
+        if CLI_input.lower() == "content":
+            print(run_write_content_menu())
+            CLI_input = input()
+            post.setdefault("content", CLI_input)
+        if CLI_input.lower() == "post":
+            PP = pprint.PrettyPrinter(indent=4)
+            PP.pprint(post)
+        if CLI_input.lower() == "publish":
+            wp_publish(CREDS, post)
+        if CLI_input.lower() == "template":
+            print(run_write_ask_template_data())
+            CLI_input = input()
+            if CLI_input == "yaml":
+                print(run_write_ask_template_filepath())
+                CLI_input = input()
+                post_template = load_template(CLI_input)
+                post.setdefault("title", post_template["title"])
+                post.setdefault("content", post_template["content"])
+                wp_publish(CREDS, post)
+            if CLI_input == "plain":
+                print(run_write_ask_template_filepath())
+                CLI_input = input()
+                template = CLI_input
+                publish_plaintext(template)
+
+        else:
+            if CLI_input.lower() == "read":
+                data = None
+                run_read(CREDS, data)
+            if CLI_input.lower() == "exit":
+                exit()
+
+
+def load_template(CLI_input):  # pragma: no cover
+    with open(CLI_input, 'r') as f:
+        post_template = yaml.safe_load(f)
+    with open('./data/post_template.json', 'w') as f:
+        json.dump(post_template, f)
+    with open('./data/post_template.json', 'r') as f:
+        post_template = json.load(f)
+    return post_template
+
+
+def publish_template(template):  # pragma: no cover
+    post = {
+        "date": get_date(),
+        "status": "publish",
+        "format": "standard"
+    }
+    with open(template, 'r') as f:
+        post_template = yaml.safe_load(f)
+    with open('./data/post_template.json', 'w') as f:
+        json.dump(post_template, f)
+    with open('./data/post_template.json', 'r') as f:
+        post_template = json.load(f)
+    post.setdefault("title", post_template["title"])
+    post.setdefault("content", post_template["content"])
+    wp_publish(CREDS, post)
+
+
+def publish_plaintext(template):  # pragma: no cover
+    post = {
+        "date": get_date(),
+        "status": "publish",
+        "format": "standard"
+    }
+    with open(template, 'r') as f:
+        lines = f.readlines()
+        post_template = [line.strip() for line in lines]
+    post.setdefault("title", post_template.pop(0))
+    post_template = '\n '.join(post_template)
+    post.setdefault("content", post_template)
+    wp_publish(CREDS, post)
+
+
+"""
+RUN PYBLOG MAIN FUNCTION
+"""
+
+
 def run_pyblog():  # pragma: no cover
     blog, data, template = parse_args()
     if blog == "read":
@@ -348,8 +414,7 @@ def run_pyblog():  # pragma: no cover
     if blog == "write" and data == "plain" and template is not None:
         publish_plaintext(template)
     else:
-        print("Please specify --blog=read or --blog=write. \
-        Please use -h or --help for more information on this tool")
+        print(run_pyblog_specify_blog_data())
 
 
 if __name__ == "__main__":
--- pyblog/test_pyblog.py
+++ pyblog/test_pyblog.py
@@ -43,6 +43,7 @@ def test_get_wp_r_timeouterror():
         r = pyblog.get_wp_r(CREDS)
         assert r is None
 
+
 def test_wp_publish_success():
     curHeaders = {
         "Authorization": "Bearer %s" % CREDS["jwt_auth"],
@@ -54,9 +55,105 @@ def test_wp_publish_success():
         mock_response = mock_post.return_value
         mock_response.status_code = 201
         r = pyblog.wp_publish(CREDS, post)
-        mock_post.assert_called_with(CREDS["wp_posts_url"], headers=curHeaders, json=post)
-        if not mock_response.status_code == 201:
-            assert "Post failed.\
+        mock_post.assert_called_with(
+            CREDS["wp_posts_url"], headers=curHeaders, json=post)
+        assert r == "Data successfully posted to wordpress."
+
+
+def test_wp_publish_failure():
+    curHeaders = {
+        "Authorization": "Bearer %s" % CREDS["jwt_auth"],
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+    }
+    post = {}
+    with patch('requests.post') as mock_post:
+        mock_response = mock_post.return_value
+        mock_response.status_code = 500
+        r = pyblog.wp_publish(CREDS, post)
+        mock_post.assert_called_with(
+            CREDS["wp_posts_url"], headers=curHeaders, json=post)
+        assert r == "Post failed.\
             Possible issue with connection or credentials."
-        else:
-            assert "Data successfully posted to wordpress."
+
+
+def test_wp_publish_connectionerror():
+    post = {}
+    with patch('requests.post') as mock_post:
+        mock_post.side_effect = exceptions.ConnectionError
+        r = pyblog.wp_publish(CREDS, post)
+        assert r == "Could not connect to wordpress."
+
+
+def test_wp_publish_timeouterror():
+    post = {}
+    with patch('requests.post') as mock_post:
+        mock_post.side_effect = exceptions.Timeout
+        r = pyblog.wp_publish(CREDS, post)
+        assert r == "Could not connect to wordpress."
+
+
+def test_run_pyblog_specify_blog_data():
+    return_string = pyblog.run_pyblog_specify_blog_data()
+    assert return_string == "Please specify --blog=read or --blog=write. \
+        Please use -h or --help for more information on this tool"
+
+
+def test_run_write_CLI_main_menu():
+    return_string = pyblog.run_write_CLI_main_menu()
+    assert return_string == "Please select from the following:\
+        title (create title for your post)\
+        content (write your post)\
+        post (show post data before publishing)\
+        publish (post to wordpress)\
+        template (publishes designated template file)\
+        read (switch to read mode)\
+        exit (quit Pyblog CLI)"
+
+
+def test_run_write_title_menu():
+    return_string = pyblog.run_write_title_menu()
+    assert return_string == "Provide a title for your post:"
+
+
+def test_run_write_content_menu():
+    return_string = pyblog.run_write_content_menu()
+    assert return_string == "Provide content for your post:"
+
+
+def test_run_write_ask_template_data():
+    return_string = pyblog.run_write_ask_template_data()
+    assert return_string == "Is template file yaml or plain text?\
+    Type yaml or plain:"
+
+
+def test_run_write_ask_template_filepath():
+    return_string = pyblog.run_write_ask_template_filepath()
+    assert return_string == "Provide filepath to template:"
+
+
+def test_run_read_main_menu():
+    return_string = pyblog.run_read_main_menu()
+    assert return_string == "Choose how to display data:\
+        json, yaml\
+        write (switch to write mode)\
+        exit (quit Pyblog CLI)"
+
+
+def test_run_read_connection_error():
+    return_string = pyblog.run_read_connection_error()
+    assert return_string == "Could not connect to wordpress."
+
+
+def test_run_read_display_json():
+    return_string = pyblog.run_read_display_json()
+    assert return_string == "Displaying newest post data.\
+        Entire response saved to ./data/r.json"
+
+
+def test_run_read_display_yaml():
+    return_string = pyblog.run_read_display_yaml()
+    assert return_string == "Displaying newest post data.\
+        Entire response saved to ./data/r.yaml\
+        Clean response saved to ./data/clean.yaml\
+        yaml data displayed saved to ./data/cleaner.yaml"
